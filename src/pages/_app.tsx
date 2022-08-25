de50cace70938ca6ccbcb75964a7c93cc11cbe97
Diapostrofo <Diapostrofo@gmail.com>
2022-08-25 22:47:50 +0200

    fix seo image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css';
+import SeoHead from '@/components/SeoHead';
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <SeoHead />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
