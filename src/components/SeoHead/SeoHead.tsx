cef020570675a34aeda2f9bb37ba3b78cfc61f4d
Diapostrofo <Diapostrofo@gmail.com>
2022-08-25 22:42:23 +0200

    seo image

---
+++ src/components/SeoHead/SeoHead.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function IngredientsTable(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title> PALEO MEAL BUDDY🍽️</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          property="og:image"
+          content="https://paleobuddy.vercel.app/images/og-cover-image.png"
+        />
+      </Head>
+    </>
+  );
+}
